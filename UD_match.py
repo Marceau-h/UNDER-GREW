c6c5e5f459e4e83635df9b669e9887afda11515d
Marceau-h <106751184+Marceau-h@users.noreply.github.com>
2023-07-12 14:23:14 +0200

    Add ud tags + stats par lemma

---
+++ UD_match.py
@@ -0,0 +1,83 @@
+import re
+from pathlib import Path
+from io import StringIO
+import pandas as pd
+
+columns = "ID FORM LEMMA UPOS XPOS FEATS HEAD DEPREL DEPS MISC".split()
+
+ud_dir = Path("UD")
+exports_dir = Path("exports")
+
+
+def conllu_to_dict(conllu):
+    conllu = conllu.split("\n")
+    conllu = [l.split("\t") for l in conllu if l != "" and not l.startswith("#")]
+    conllu = [dict(zip(columns, l)) for l in conllu]
+    # conllu = {l[0]: l for l in conllu}
+    return conllu
+
+
+for subfolder in ud_dir.iterdir():
+    if not subfolder.is_dir():
+        print(f"{subfolder} is not a folder")
+        continue
+
+    all_txt = StringIO()
+    for connlu in subfolder.glob("*.conllu"):
+        # print(connlu)
+        with open(connlu, "r") as f:
+            all_txt.write(f.read())
+
+    sents = [s.split("# sent_id = ")[-1] for s in all_txt.getvalue().split("\n\n") if s != ""]
+    sents = [s.split("\n", 1) for s in sents]
+    sents = {s[0]: conllu_to_dict(s[1]) for s in sents}
+
+    exports_sub = exports_dir / subfolder.name
+
+    for export in exports_sub.glob("*.csv"):
+        df = pd.read_csv(export).fillna("")
+
+        pivot_datas = []
+        for line, row in df.iterrows():
+            sent = sents[row["sent_id"]]
+            left = row["left_context"]
+            pivot = row["pivot"]
+            try:
+                # count = left.count(pivot) if left != "" else 0
+                count = sum(1 for e in left if e == pivot) - sum(1 for e in re.findall(fr"\w*{pivot}\w*", left) if e)
+                # print(count)
+                # print(left)
+                count = count if count >= 0 else 0
+
+            except:
+                print(left)
+                print(pivot)
+                raise
+
+            # print([e[1] for e in sent])
+            # print(pivot)
+
+            words = [e["FORM"] for e in sent]
+            try:
+                token_nb = words.index(pivot, count)
+            except:
+                print(f"{words = }")
+                print(f"{pivot = }")
+                print(f"{count = }")
+                raise
+            # print(sent[token_nb])
+
+            pivot_data = sent[token_nb]
+
+            pivot_datas.append(pivot_data)
+
+        df_pivot = pd.DataFrame(pivot_datas)
+
+        df = df.join(df_pivot)
+
+        df.to_csv(export, index=False)
+        df.to_csv(export.with_suffix('.tsv'), sep="\t", index=False)
+        df.to_excel(export.with_suffix('.xlsx'), index=False)
+        df.to_pickle(export.with_suffix('.pkl'))
+        df.to_json(export.with_suffix('.jsonl'), orient='records', lines=True)
+        df.to_json(export.with_suffix('.json'), orient='records')
--- XLStats.py
+++ XLStats.py
@@ -3,69 +3,74 @@ import pandas as pd
 from collections import Counter
 
 main = Path("exports")
-stats_folder = Path("stats")
-jsons = list(main.glob("*/*.json"))
-nb_feuilles = 10
+stats_folder_main = Path("stats")
+for mode in ("pivot", "LEMMA"):
+    stats_folder = stats_folder_main / mode
+    jsons = list(main.glob("*/*.json"))
+    nb_feuilles = 10
 
-stats = {}
+    stats = {}
 
-for json in jsons:
-    corpus = json.parent.name
-    query = json.stem
+    for json in jsons:
+        corpus = json.parent.name
+        query = json.stem
 
-    stats_file = stats_folder / corpus
-    stats_file.mkdir(parents=True, exist_ok=True)
-    stats_file = stats_file / (query + ".xlsx")
+        stats_file = stats_folder / corpus
+        stats_file.mkdir(parents=True, exist_ok=True)
+        stats_file = stats_file / (query + ".xlsx")
 
-    if corpus not in stats:
-        stats[corpus] = {}
-    stats[corpus][query] = {}
+        if corpus not in stats:
+            stats[corpus] = {}
+        stats[corpus][query] = {}
 
-    df = pd.read_json(json)
-    verbes = df["pivot"]
-    verbes_freq = Counter(verbes)
-    occurences = len(verbes)
+        df = pd.read_json(json)
+        verbes = df[mode]
+        verbes_freq = Counter(verbes)
+        occurences = len(verbes)
 
-    # print(df.head())
+        # print(df.head())
 
-    temp = {
-        "nb_occurences": occurences,
-        "nb_verbes": len(verbes.unique()),
+        tuplverbes = sorted([(v, verbes_freq[v]) for v in verbes_freq], key=lambda x: x[1], reverse=True)
+        tuplverbes = {v: c for v, c in tuplverbes}
 
-        "verbes_freq": ", ".join(sorted((f"{v}:{verbes_freq[v]}" for v in verbes_freq), key=lambda x: x[1], reverse=True)),
-    }
+        temp = {
+            "nb_occurences": occurences,
+            "nb_verbes": len(verbes.unique()),
+            **tuplverbes,
+            }
 
-    feuilles = {}
+        feuilles = {}
 
-    for verbe, count in verbes_freq.most_common(nb_feuilles):
-        phrases = df[df["pivot"] == verbe]
-        feuilles[verbe] = {}
-        feuilles[verbe]["stats"] = {
-            "nb_occurences": count,
-            "freq": count / occurences,
-            "nb_phrases": len(phrases),
-        }
+        for verbe, count in verbes_freq.most_common(nb_feuilles):
+            phrases = df[df[mode] == verbe]
+            feuilles[verbe] = {}
+            feuilles[verbe]["stats"] = {
+                "nb_occurences": count,
+                "freq": count / occurences,
+                "nb_phrases": len(phrases),
+            }
 
-        # [(sent_id, left, pivot, right) for sent_id, left, pivot, right in phrases[["sent_id", "left_context", "pivot", "right_context"]].values]
-        feuilles[verbe]["phrases"] = phrases[["sent_id", "left_context", "pivot", "right_context"]].values.tolist()
+            # [(sent_id, left, pivot, right) for sent_id, left, pivot, right in phrases[["sent_id", "left_context", "pivot", "right_context"]].values]
+            feuilles[verbe]["phrases"] = phrases.values.tolist()
 
-    with pd.ExcelWriter(stats_file, engine='xlsxwriter') as writer:
+        with pd.ExcelWriter(stats_file, engine='xlsxwriter') as writer:
 
-        tempdf = pd.DataFrame(temp, index=[0])
-        tempdf.to_excel(writer, index=False, sheet_name="Globales")
+            tempdf = pd.DataFrame(temp, index=["", ])
+            tempdf = tempdf.T
+            tempdf.to_excel(writer, sheet_name="Globales", index=True, header=False)
 
-        for verbe, content in feuilles.items():
-            tempdf = pd.DataFrame(content["stats"], index=[0])
-            tempdf.to_excel(writer, index=False, sheet_name=f"{verbe}_stats")
+            for verbe, content in feuilles.items():
+                tempdf = pd.DataFrame(content["stats"], index=[0])
+                tempdf.to_excel(writer, index=False, sheet_name=f"{verbe}_stats")
 
-            tempdf = pd.DataFrame(content["phrases"], columns=["sent_id", "left_context", "pivot", "right_context"])
-            tempdf.to_excel(writer, index=False, sheet_name=f"{verbe}_phrases")
-
-
-    temp["feuilles"] = feuilles
+                tempdf = pd.DataFrame(content["phrases"], columns=phrases.columns)
+                tempdf.to_excel(writer, index=False, sheet_name=f"{verbe}_phrases")
 
 
-    stats[corpus][query] = temp
+        temp["feuilles"] = feuilles
+
+
+        stats[corpus][query] = temp
 
 
 
