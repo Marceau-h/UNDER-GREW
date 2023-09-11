c94a4657e79686120983e8e9b2193146ca3d3399
Marceau-h <106751184+Marceau-h@users.noreply.github.com>
2023-09-11 16:45:49 +0200

    WAConnlu concurrent tests

--- Concurrent_WAConnlu2.py
+++ Concurrent_WAConnlu2.py
@@ -1,17 +1,20 @@
 import re
+from io import StringIO
+from pathlib import Path
+from multiprocessing import Pool, cpu_count
 
 import spacy
 from tqdm.auto import tqdm
-from io import StringIO
-from pathlib import Path
 
-WAC = Path("UD/WAC")
+WAC = Path("UD/WAC_test")
 WAC.mkdir(exist_ok=True, parents=True)
 
 nlp = spacy.load("fr_core_news_sm")
+# spacy.prefer_gpu()
+# nlp = spacy.load("fr_dep_news_trf")
 
-# file = "/home/marceau/Téléchargements/fra_mixed_2009_1M/fra_mixed_2009_1M-sentences.txt"
-file = r"C:\Users\marce\Downloads\fra_mixed_2009_1M\fra_mixed_2009_1M-sentences.txt"
+file: Path = Path("/home/marceau/Téléchargements/fra_mixed_2009_1M/fra_mixed_2009_1M-sentences.txt")
+# file: Path = Path(r"C:\Users\marce\Downloads\fra_mixed_2009_1M\fra_mixed_2009_1M-sentences.txt")
 
 
 def clean(s: str) -> str:
@@ -26,7 +29,7 @@ def no_empty(s: str) -> str:
     return s if s else "_"
 
 
-def get_all(sent):
+def get_all(sent: str) -> list[dict]:
     doc = nlp(sent)
     deps = [token.dep_.lower() for token in doc]
     return [
@@ -46,36 +49,26 @@ def get_all(sent):
     ]
 
 
-with open(file, "r", encoding="utf-8") as f:
-    lines = f.readlines()
-
-# lines = lines[:10000]
-segments = []
-len_seg = 30_000
-for i in range(0, len(lines), len_seg):
-    end = min(len(lines), i + len_seg)
-    segments.append(lines[i:end])
-
-batch_first_sent_id = 1
-for i, segment in enumerate(segments):
-    batch_last_sent_id = batch_first_sent_id + len(segment) - 1
-
-    pbar = tqdm(segment, total=len(segment), desc=f"Fichier {i + 1}/{len(segments)} : ", leave=False)
+def process_segment(segment: dict) -> None:
+    first = None
 
     srtio = StringIO()
     srtio.write("# global.columns = ID FORM LEMMA UPOS XPOS FEATS HEAD DEPREL DEPS MISC\n")
-    for j, l in enumerate(pbar):
-        starting_line = l
-        _, l = l.rsplit("\t", 1)
+    for i, l in segment.items():
+        lid = int(i)
+
+        if first is None:
+            first = lid
+            # print(f"First: {first}")
 
         l = clean(l)
 
         if not l:
-            print(f"Empty line at {batch_first_sent_id + j}")
-            print(starting_line)
+            print(f"Empty line at {lid}")
+            print(l)
             raise ValueError
 
-        srtio.write(f"# sent_id = {batch_first_sent_id + j}\n")
+        srtio.write(f"# sent_id = {lid}\n")
         # srtio.write(f"# text = {l[1]}\n")
         srtio.write(f"# text = {l}\n")
         # for token in get_all(l[1]):
@@ -83,7 +76,28 @@ for i, segment in enumerate(segments):
             srtio.write("\t".join([str(v) for v in token.values()]) + "\n")
         srtio.write("\n")
 
-    with open(WAC / f"{batch_first_sent_id}_{batch_last_sent_id}.conllu", "w", encoding="utf-8") as f:
+    with open(WAC / f"{first}_{lid}.conllu", "w", encoding="utf-8") as f:
         f.write(srtio.getvalue())
 
-    batch_first_sent_id = batch_last_sent_id + 1
+
+with file.open("r", encoding="utf-8") as f:
+    # The `int(i)` allows us to check if we are catching the sentence id correctly
+    lines: dict = {int(i): l for i, l in [l.rsplit("\t", 1) for l in f.readlines()]}
+
+# lines = lines[:10000] ## For testing
+len_seg = 30_000
+segments: list[dict] = []
+for i in range(0, len(lines), len_seg):
+    sub = {k: lines[k] for k in range(i, i + len_seg) if k in lines}
+    segments.append(sub)
+
+# with Pool(cpu_count()//2) as p:
+#     p.starmap(
+#         process_segment,
+#         [(segment,) for segment in segments],
+#     )
+
+with Pool(cpu_count()//2) as p:
+    # p.map(process_segment, segments)
+    list(tqdm(p.imap(process_segment, segments), total=len(segments)))
+
