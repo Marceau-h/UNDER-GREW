d4a744aae021305b19a0fcf9ec5ad40feb295c04
Marceau-h <106751184+Marceau-h@users.noreply.github.com>
2023-09-12 17:20:00 +0200

    concurrent ud match

--- 03_Concurrent_UD_match.py
+++ 03_Concurrent_UD_match.py
@@ -1,6 +1,7 @@
 import re
 from pathlib import Path
 from io import StringIO
+from multiprocessing import Pool, cpu_count
 
 import pandas as pd
 from tqdm.auto import tqdm
@@ -46,7 +47,59 @@ def dist_name(right: str, left: str) -> int:
     return min(last_name(right), first_name(left))
 
 
-for subfolder in tqdm(list(ud_dir.iterdir())):
+def process_row(args: tuple[pd.Series, list[dict[str, str]]]) -> dict[str, str]:
+    row, sent = args
+    
+    left = row["left_context"]
+    left = left if not (isinstance(left, float) or pd.isna(left)) else ""
+    pivot = row["pivot"]
+    pivot = pivot if not isinstance(pivot, float) else ""
+    pivot = pivot.replace('""', '"')
+    right = row["right_context"]
+    right = right if not isinstance(right, float) else ""
+
+    dist = dist_name(right, left)
+
+    if pivot == "":
+        print(f"{row['sent_id'] = }")
+        print(f"{subfolder.name = }")
+        print(f"{left = }")
+        print(f"{pivot = }")
+        print("pivot is empty")
+        return {}
+
+    try:
+        count = (sum(1 for e in re.findall(fr"\W{re.escape(pivot)}\W", right) if e)
+                 + sum(1 for e in re.findall(fr"^{re.escape(pivot)}(\b)", left) if e)
+                 + sum(1 for e in re.findall(fr"(\b){re.escape(pivot)}$", left) if e))
+        count = count if count >= 0 else 0
+
+    except:
+        print(left)
+        print(pivot)
+        raise
+
+    words = [e["FORM"] for e in sent]
+    try:
+        token_nb = words.index(pivot, count)
+    except ValueError:
+        try:
+            token_nb = words.index(pivot)
+        except ValueError:
+            print(row)
+            print(f"{words = }")
+            print(f"{pivot = }")
+            print(f"{count = }")
+            print(f"{subfolder.name = }")
+            raise
+
+    pivot_data = sent[token_nb]
+    pivot_data["dist"] = dist
+
+    return pivot_data
+
+
+for subfolder in (list(ud_dir.iterdir())):
     print(f"{subfolder.name}")
 
     if not subfolder.is_dir():
@@ -73,7 +126,8 @@ for subfolder in tqdm(list(ud_dir.iterdir())):
     for export in exports_sub.glob("*.csv"):
         df = pd.read_csv(export, low_memory=False)
         pivot_datas = []
-        for line, row in df.iterrows():
+        args = []
+        for _, row in df.iterrows():
             try:
                 sent = sents[str(row["sent_id"])]
             except Exception as e:
@@ -82,52 +136,13 @@ for subfolder in tqdm(list(ud_dir.iterdir())):
                 print(sents)
                 raise
 
-            left = row["left_context"]
-            left = left if not (isinstance(left, float) or pd.isna(left)) else ""
-            pivot = row["pivot"]
-            pivot = pivot if not isinstance(pivot, float) else ""
-            pivot = pivot.replace('""', '"')
-            right = row["right_context"]
-            right = right if not isinstance(right, float) else ""
+            args.append((row, sent))
 
-            dist = dist_name(right, left)
-
-            if pivot == "":
-                print(f"{row['sent_id'] = }")
-                print(f"{subfolder.name = }")
-                print(f"{left = }")
-                print(f"{pivot = }")
-                print("pivot is empty")
-                continue
-            try:
-                count = (sum(1 for e in re.findall(fr"\W{re.escape(pivot)}\W", right) if e)
-                         + sum(1 for e in re.findall(fr"^{re.escape(pivot)}(\b)", left) if e)
-                         + sum(1 for e in re.findall(fr"(\b){re.escape(pivot)}$", left) if e))
-                count = count if count >= 0 else 0
-
-            except:
-                print(left)
-                print(pivot)
-                raise
-
-            words = [e["FORM"] for e in sent]
-            try:
-                token_nb = words.index(pivot, count)
-            except ValueError:
-                try:
-                    token_nb = words.index(pivot)
-                except ValueError:
-                    print(row)
-                    print(f"{words = }")
-                    print(f"{pivot = }")
-                    print(f"{count = }")
-                    print(f"{subfolder.name = }")
-                    raise
-
-            pivot_data = sent[token_nb]
-            pivot_data["dist"] = dist
-
-            pivot_datas.append(pivot_data)
+        with Pool(cpu_count()) as p:
+            pbar = tqdm(p.imap(process_row, args), total=len(args))
+            # pbar =p.imap(process_row, args)
+            for res in pbar:
+                pivot_datas.append(res)
 
         df_pivot = pd.DataFrame(pivot_datas)
 
